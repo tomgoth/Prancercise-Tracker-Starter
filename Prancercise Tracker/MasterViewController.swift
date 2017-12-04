e85f272d2a65eed1bed4007d02e61bece82d07e5
tom goth <dominiquemaack@gmail.com>
2017-12-03 22:49:20 -0700

    first commit

--- Prancercise Tracker/MasterViewController.swift
+++ Prancercise Tracker/MasterViewController.swift
@@ -37,6 +37,23 @@ class MasterViewController: UITableViewController {
   private let authorizeHealthKitSection = 2
   
   private func authorizeHealthKit() {
+    HealthKitSetupAssistant.authorizeHealthKit { (authorized, error) in
+        
+        guard authorized else {
+            
+            let baseMessage = "HealthKit Authorization Failed"
+            
+            if let error = error {
+                print("\(baseMessage). Reason: \(error.localizedDescription)")
+            } else {
+                print(baseMessage)
+            }
+            
+            return
+        }
+        
+        print("HealthKit Successfully Authorized.")
+    }
 
   }
   
